dd7423b159b8676581e66a205bf46efbe6e26625
Janith Ishara <115034081+gmjishara@users.noreply.github.com>
2022-12-01 00:33:42 +0530

    Add files via upload

---
+++ D3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class D3{
+	public static void main(String arg[]){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		int a=input.nextInt();
+		long fact=1;
+		for(int i=a;i>0;i--){
+			fact*=i;
+		}
+		System.out.println("The Factorial of "+a+" is: "+fact);
+	}
+}
+
